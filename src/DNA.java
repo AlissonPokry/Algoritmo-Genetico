ab3ee47749d701477937e624adadc59ea0b5ff03
Alisson <alissonpokrysilva@gmail.com>
2025-06-11 20:12:08 -0300

    first commit

---
+++ src/DNA.java
@@ -0,0 +1,68 @@
+
+public class DNA {
+	public static final int ngenes = 40;//979;
+	public static final int nivelMutacao = 10;
+	int odna[] = new int[ngenes];
+	float score = -1;
+	float distanciaTotal = -1;
+	int repetidos = -1;
+	
+	public void criarandomico() {
+		for(int i = 0; i < ngenes; i++) {
+			odna[i] = Main.rnd.nextInt(ngenes);
+		}
+	}
+	
+	public void criaPaiMae(DNA pai,DNA mae,int cossover) {
+		for(int i = 0; i < ngenes; i++) {
+			if(i < cossover) {
+				odna[i] = pai.odna[i];
+			}else {
+				odna[i] = mae.odna[i];
+			}
+		}
+		
+		for(int i = 0; i < 5; i++) {
+			int index = Main.rnd.nextInt(ngenes);
+			odna[index] = Main.rnd.nextInt(ngenes);
+		}
+		
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder sdna = new StringBuilder();
+		sdna.append("[");
+		for(int i = 0; i < ngenes; i++) {
+			if(i>0) {
+				sdna.append(",");
+			}
+			sdna.append(odna[i]);
+		}
+		sdna.append("]");
+		return sdna.toString();
+	}
+	
+	public void avaliaDna() {
+		Float somaValor = 0.0f;
+		int repetidos = 0;
+		int visitados[] = new int[ngenes];
+		visitados[odna[0]] = 1;
+		for(int i = 1; i < ngenes; i++) {
+			int cod1 = odna[i-1];
+			int cod2 = odna[i];
+			if(visitados[cod2]==1) {
+				repetidos++;
+			}else {
+				visitados[cod2]=1;
+			}
+			
+			float dist = Main.matrizdistancias[cod1][cod2];
+			somaValor+=dist;
+		}
+		
+		distanciaTotal = somaValor;
+		this.repetidos = repetidos;
+		score = somaValor + repetidos*100;
+	}	
+}
---
+++ src/Main.java
@@ -0,0 +1,330 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Random;
+
+class Cotacao {
+    String data;
+    String codigo;
+    double preco;
+    
+    public Cotacao(String data, String codigo, double preco) {
+        this.data = data;
+        this.codigo = codigo;
+        this.preco = preco;
+    }
+}
+
+public class Main {
+	public static float matrizdistancias[][];
+	public static ArrayList<float[]> listadelinhas = new ArrayList<float[]>();
+	public static int citycode = 0;
+	public static HashMap<Integer,String> mapCodName = new HashMap<Integer,String>();
+	public static HashMap<String,Integer> mapNameCod = new HashMap<String,Integer>();
+	public static Random rnd = new Random();
+	public static int numerodeindividuos = 1000000;
+	public static String NOME_ARQUIVO = "cotacoes.csv";
+	public static final int NUMERO_POTES = 10;
+	public static int montante = 100000; //dinheiro deve SEMPRE ser salvo como Int em centavos
+
+
+	public static void main(String[] args) {
+	
+
+		// carregaMatrizDistancia();
+		
+		// ArrayList<DNA> pollGenetico = new ArrayList<DNA>();
+		
+		// for(int i = 0; i < numerodeindividuos; i++) {
+		// 	DNA dna1 = new DNA();
+		// 	dna1.criarandomico();
+		// 	dna1.avaliaDna();
+		// 	pollGenetico.add(dna1);
+		// }
+		
+		// int numerogeracoes = 100;
+		// for(int geracao = 0; geracao < numerogeracoes; geracao++) {
+			
+		// 	Collections.sort(pollGenetico, new Comparator<DNA>() {
+		// 		@Override
+		// 		public int compare(DNA o1, DNA o2) {
+		// 			return o1.score < o2.score? -1: o1.score > o2.score? 1:0;
+		// 		}
+		// 	});
+			
+		// 	System.out.println("Geracao "+geracao);
+		// 	for(int i = 0; i < 10;i++) {
+		// 		DNA dna = pollGenetico.get(i);
+		// 		System.out.println(""+i+": "+dna.score+","+dna.distanciaTotal+","+dna.repetidos+" "+dna);
+		// 	}
+			
+		// 	ArrayList<DNA> novosGenes = new ArrayList<DNA>();
+		// 	for(int i = 0; i < numerodeindividuos; i++) {
+		// 		int pai = sorteio();
+		// 		int mae = sorteio();
+				
+		// 		if(pai==mae) {
+		// 			mae++;
+		// 		}
+				
+		// 		DNA dna_pai = pollGenetico.get(pai);
+		// 		DNA dna_mae = pollGenetico.get(mae);
+				
+		// 		int pontocrossover = rnd.nextInt(DNA.ngenes-10)+5;
+				
+		// 		DNA novodna = new DNA();
+		// 		novodna.criaPaiMae(dna_pai, dna_mae, pontocrossover);
+		// 		novosGenes.add(novodna);
+		// 	}
+			
+		// 	pollGenetico.clear();
+		// 	for(int i = 0; i < numerodeindividuos; i++) {
+		// 		DNA dna = novosGenes.get(i);
+		// 		dna.avaliaDna();
+		// 		pollGenetico.add(dna);
+		// 	}
+		// }
+
+		negociarAcoes();
+		System.out.println("\nNegociações concluídas.");
+		
+	}
+
+	public static int[] dividirMontanteEmPotes() {
+        int[] potes = new int[NUMERO_POTES];
+        int valorPorPote = montante / NUMERO_POTES;
+        
+        for (int i = 0; i < NUMERO_POTES; i++) {
+            potes[i] = valorPorPote;
+        }
+        
+        // Adiciona os centavos restantes da divisão ao último pote
+        int resto = montante % NUMERO_POTES;
+        if (resto > 0) {
+            potes[NUMERO_POTES - 1] += resto;
+        }
+        
+        // Imprime o valor de cada pote
+        for (int i = 0; i < NUMERO_POTES; i++) {
+            System.out.printf("Pote %d: R$ %.2f\n", i + 1, potes[i] / 100.0);
+        }
+        
+        return potes;
+    }
+
+	public static ArrayList<Cotacao> getCotacoes(String nomeArquivo) {
+    ArrayList<Cotacao> cotacoes = new ArrayList<>();
+    try {
+        BufferedReader bfr = new BufferedReader(new FileReader(nomeArquivo));
+        String linha;
+
+        bfr.readLine(); // Pula o cabeçalho
+
+        while ((linha = bfr.readLine()) != null) {
+            String[] dados = linha.split(";");
+            
+            if (dados.length >= 3) {
+                String data = dados[0].split(" ")[0];
+                String codigo = dados[1];
+                
+                // Remove o F do final do código se existir
+                if (codigo.endsWith("F")) {
+                    codigo = codigo.substring(0, codigo.length() - 1);
+                }
+                
+                double fechamento = Double.parseDouble(dados[2].replace(",", "."));
+                
+                cotacoes.add(new Cotacao(data, codigo, fechamento));
+            }
+        }
+        bfr.close();
+    } catch (Exception e) {
+        System.out.println("Erro ao ler arquivo: " + e.getMessage());
+    }
+    return cotacoes;
+}
+
+public static void negociarAcoes() {
+    ArrayList<Cotacao> cotacoes = getCotacoes(NOME_ARQUIVO);
+    
+    // Agrupa as cotações por data
+    HashMap<String, ArrayList<Cotacao>> cotacoesPorData = new HashMap<>();
+    for (Cotacao c : cotacoes) {
+        cotacoesPorData.computeIfAbsent(c.data, k -> new ArrayList<>()).add(c);
+    }
+    
+    // Ordena as datas
+    ArrayList<String> datas = new ArrayList<>(cotacoesPorData.keySet());
+    Collections.sort(datas);
+    
+    boolean diaCompra = true;
+    int[] potes = dividirMontanteEmPotes();
+    // Mapa para armazenar ações de cada pote separadamente
+    HashMap<Integer, HashMap<String, Integer>> acoesPorPote = new HashMap<>();
+    String ultimoDiaCompra = null;
+    
+    System.out.println("\nIniciando negociações...");
+    
+    for (String data : datas) {
+        ArrayList<Cotacao> cotacoesDoDia = cotacoesPorData.get(data);
+        
+        if (diaCompra) {
+            // Dia de compra
+            System.out.println("\nDia " + data + " - COMPRANDO");
+            acoesPorPote.clear(); // Limpa ações anteriores
+            
+            for (int i = 0; i < NUMERO_POTES; i++) {
+                if (i < cotacoesDoDia.size()) {
+                    Cotacao cotacao = cotacoesDoDia.get(i);
+                    int quantidade = (int)(potes[i] / (cotacao.preco * 100));
+                    int valorGasto = (int)(quantidade * cotacao.preco * 100);
+                    potes[i] -= valorGasto;
+                    
+                    // Armazena as ações compradas com o dinheiro deste pote
+                    HashMap<String, Integer> acoesDoPote = new HashMap<>();
+                    acoesDoPote.put(cotacao.codigo, quantidade);
+                    acoesPorPote.put(i, acoesDoPote);
+                    
+                    System.out.printf("Pote %d: Comprado %d ações de %s a R$ %.2f cada (Total: R$ %.2f)\n",
+                        i + 1, quantidade, cotacao.codigo, cotacao.preco, valorGasto/100.0);
+                }
+            }
+            ultimoDiaCompra = data;
+            
+        } else {
+            // Dia de venda - apenas se houver dia de compra anterior
+            if (ultimoDiaCompra != null && !acoesPorPote.isEmpty()) {
+                System.out.println("\nDia " + data + " - VENDENDO");
+                int valorTotal = 0;
+                
+                // Vende as ações de cada pote separadamente
+                for (int i = 0; i < NUMERO_POTES; i++) {
+                    HashMap<String, Integer> acoesDoPote = acoesPorPote.get(i);
+                    if (acoesDoPote != null) {
+                        int valorPote = 0;
+                        
+                        for (String codigoAcao : acoesDoPote.keySet()) {
+                            // Procura a cotação da ação neste dia
+                            Cotacao cotacaoDaAcao = null;
+                            for (Cotacao c : cotacoesDoDia) {
+                                if (c.codigo.equals(codigoAcao)) {
+                                    cotacaoDaAcao = c;
+                                    break;
+                                }
+                            }
+                            
+                            if (cotacaoDaAcao != null) {
+                                int quantidade = acoesDoPote.get(codigoAcao);
+                                int valorVenda = (int)(quantidade * cotacaoDaAcao.preco * 100);
+                                valorPote += valorVenda;
+                                
+                                System.out.printf("Pote %d: Vendido %d ações de %s a R$ %.2f cada (Total: R$ %.2f)\n",
+                                    i + 1, quantidade, codigoAcao, cotacaoDaAcao.preco, valorVenda/100.0);
+                            } else {
+                                System.out.printf("AVISO: Não foi possível vender ações do pote %d: %s (cotação não encontrada)\n", 
+                                    i + 1, codigoAcao);
+                            }
+                        }
+                        valorTotal += valorPote;
+                        potes[i] = valorPote; // Atualiza o valor do pote
+                    }
+                }
+                
+                montante = valorTotal;
+                System.out.printf("Novo montante total: R$ %.2f\n\n", montante/100.0);
+                potes = dividirMontanteEmPotes(); // Redistribui o montante entre os potes
+            }
+        }
+        
+        diaCompra = !diaCompra;
+    }
+}
+	
+
+	//----------------------------------------------------------------------------------------------
+	
+	public static int sorteio() {
+		int sorteio = rnd.nextInt(2048-1);
+		int vbase = 1024;
+		for(int i = 0; i < 10; i++) {
+			sorteio-=vbase;
+			if(sorteio<0) {
+				return i;
+			}
+			vbase = vbase/2;
+		}
+		return 9;
+	}
+
+	private static void carregaMatrizDistancia() {
+		BufferedReader bfr;
+		try {
+			bfr = new BufferedReader(new FileReader("distances.csv"), 1024000);
+			String header = bfr.readLine();
+			System.out.println("header "+header);
+			String line = "";
+			
+
+			while((line=bfr.readLine())!=null) {
+				//System.out.println(""+line);
+				String splt[] = line.split(",");
+				String cidade_origem = splt[0];
+				String cidade_destino = splt[1];
+				float dist = Float.parseFloat(splt[3]);
+				
+				int origemcode = -1;
+				if(mapNameCod.containsKey(cidade_origem)) {
+					origemcode = mapNameCod.get(cidade_origem);
+				}else {
+					origemcode = citycode;
+					mapNameCod.put(cidade_origem, origemcode);
+					mapCodName.put( origemcode, cidade_origem);
+					citycode++;
+				}
+				
+				int destinocode = -1;
+				if(mapNameCod.containsKey(cidade_destino)) {
+					destinocode = mapNameCod.get(cidade_destino);
+				}else {
+					destinocode = citycode;
+					mapNameCod.put(cidade_destino, destinocode);
+					mapCodName.put( destinocode, cidade_destino);
+					citycode++;
+				}
+				
+				float dado[] = new float[3];
+				dado[0] = origemcode;
+				dado[1] = destinocode;
+				dado[2] = dist;
+				
+				listadelinhas.add(dado);
+			}
+			
+			System.out.println(" "+citycode+" "+listadelinhas.size());
+			
+			matrizdistancias = new float[citycode][citycode];
+			
+			for(int i = 0; i < listadelinhas.size();i++) {
+				float dado[] = listadelinhas.get(i);
+				matrizdistancias[(int)dado[0]][(int)dado[1]] = dado[2];
+			}
+			
+			//Lenningen,Asselborn,62.3,85.5
+//			int cod1 = mapNameCod.get("Lenningen");
+//			int cod2 = mapNameCod.get("Asselborn");
+//			float dist = matrizdistancias[cod1][cod2];
+			
+			//System.out.println("Lenningen ->  Asselborn "+cod1+" - "+cod2+" dist "+dist);
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
